72bebf9356808914a66ffb9b4b2f594b4868b952
sanadkam <sanadkam@hotmail.com>
2026-03-28 22:32:41 +0100

    _app.js: add PostHog analytics + page tracking

--- viral-hook-tester/pages/_app.js
+++ viral-hook-tester/pages/_app.js
@@ -1,5 +1,31 @@
 import '../styles/globals.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+
+if (typeof window !== 'undefined') {
+  posthog.init('phc_7X74oAjFi2IWdNSncip4ut8ry6DcXKTruiwHP626iZG', {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'identified_only',
+    capture_pageview: false,
+  });
+}
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => posthog.capture('$pageview');
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
+  return (
+    <PostHogProvider client={posthog}>
+      <Component {...pageProps} />
+    </PostHogProvider>
+  );
 }
